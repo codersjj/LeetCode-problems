0cd7a269a05784b26fe93d8b4d7ebfbd1643e923
codersjj <shanewestlife@outlook.com>
2024-03-23 12:07:09 +0800

    feat: 125. Valid Palindrome

---
+++ 125.valid-palindrome.js
@@ -0,0 +1,72 @@
+/*
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+
+Example 1:
+
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+Example 2:
+
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+Example 3:
+
+Input: s = " "
+Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+Since an empty string reads the same forward and backward, it is a palindrome.
+
+
+Constraints:
+
+1 <= s.length <= 2 * 105
+s consists only of printable ASCII characters.
+*/
+
+// /**
+//  * @param {string} s
+//  * @return {boolean}
+//  */
+// var isPalindrome = function(s) {
+//   const stack = []
+//   const tokens = s.split('')
+//   tokens.forEach(token => {
+//     if (/[a-zA-Z0-9]/.test(token)) {
+//       stack.push(token.toLowerCase())
+//     }
+//   })
+//   forwardStr = stack.join('')
+//   backwardStr = stack.reverse().join('')
+//   return forwardStr === backwardStr
+// };
+
+// /**
+//  * @param {string} s
+//  * @return {boolean}
+//  */
+// var isPalindrome = function(s) {
+//   const str = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase()
+//   const backwardStr = str.split('').reverse().join('')
+//   return str === backwardStr
+// };
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+  const str = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase()
+  let left = 0
+  let right = str.length - 1
+  while (left < right) {
+    if (str[left] !== str[right]) return false
+    left++
+    right--
+  }
+  return true
+};
