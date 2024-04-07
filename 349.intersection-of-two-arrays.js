86b72a1fad20e31227491ddf18ba611aebb3d4bc
codersjj <shanewestlife@outlook.com>
2024-04-07 23:19:24 +0800

    feat: 349. Intersection of Two Arrays Approach 1: Sorting and Two Pointers

---
+++ 349.intersection-of-two-arrays.js
@@ -0,0 +1,45 @@
+/*
+Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
+
+
+Example 1:
+
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2]
+Example 2:
+
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [9,4]
+Explanation: [4,9] is also accepted.
+
+
+Constraints:
+
+1 <= nums1.length, nums2.length <= 1000
+0 <= nums1[i], nums2[i] <= 1000
+*/
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+  const orderedNums1 = [...nums1].sort((x, y) => x - y)
+  const orderedNums2 = [...nums2].sort((x, y) => x - y)
+  const result = []
+  let i = 0
+  let j = 0
+  while (i < orderedNums1.length && j < orderedNums2.length) {
+    if (orderedNums1[i] === orderedNums2[j] && !result.includes(orderedNums1[i])) {
+      result.push(orderedNums1[i])
+      i++
+      j++
+    } else if (orderedNums1[i] < orderedNums2[j]) {
+      i++
+    } else {
+      j++
+    }
+  }
+  return result
+};
