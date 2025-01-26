846a24b15d560b7b3600a99db90db1992ad96b85
codersjj <shanewestlife@outlook.com>
2025-01-26 16:27:20 +0800

    feat: 26. Remove Duplicates from Sorted Array Approach 2

--- array/26.remove-duplicates-from-sorted-array.js
+++ array/26.remove-duplicates-from-sorted-array.js
@@ -74,3 +74,20 @@ var removeDuplicates = function(nums) {
   }
   return left + 1
 };
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+  let left = 1
+
+  for (let right = 1; right < nums.length; right++) {
+    if (nums[right] !== nums[right - 1]) {
+      nums[left] = nums[right]
+      left++
+    }
+  }
+
+  return left
+};
