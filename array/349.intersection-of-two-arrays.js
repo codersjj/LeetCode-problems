aa6c4e72c093cdc1bad0f89b8e2133333c21ed7c
codersjj <shanewestlife@outlook.com>
2025-02-04 16:18:52 +0800

    feat: 349. Intersection of Two Arrays

--- array/349.intersection-of-two-arrays.js
+++ array/349.intersection-of-two-arrays.js
@@ -75,3 +75,22 @@ Constraints:
 var intersection = function(nums1, nums2) {
   return [...new Set(nums1.filter(num => nums2.includes(num)))]
 };
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+  const seen = new Set(nums1)
+  const res = []
+
+  for (const n of nums2) {
+    if (seen.has(n)) {
+      res.push(n)
+      seen.delete(n)
+    }
+  }
+
+  return res
+};
