29f742b1c4563f300fcc9ffaf1bd17267b4b1619
codersjj <shanewestlife@outlook.com>
2025-03-21 17:31:00 +0800

    feat: 94. Binary Tree Inorder Traversal

---
+++ tree/94.binary-tree-inorder-traversal.js
@@ -0,0 +1,103 @@
+/*
+94. Binary Tree Inorder Traversal
+
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+
+
+Example 1:
+
+Input: root = [1,null,2,3]
+
+Output: [1,3,2]
+
+Explanation:
+
+
+
+Example 2:
+
+Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+
+Output: [4,2,6,5,7,1,3,9,8]
+
+Explanation:
+
+
+
+Example 3:
+
+Input: root = []
+
+Output: []
+
+Example 4:
+
+Input: root = [1]
+
+Output: [1]
+
+
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+
+
+Follow up: Recursive solution is trivial, could you do it iteratively?
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+  if (!root) return []
+
+  const res = []
+
+  function dfs(node) {
+    if (!node) return
+    dfs(node.left)
+    res.push(node.val)
+    dfs(node.right)
+  }
+
+  dfs(root)
+
+  return res
+};
+
+// or:
+
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+  if (!root) return []
+  const res = []
+  const stack = []
+  let cur = root
+
+  while (cur || stack.length) {
+    while (cur) {
+      stack.push(cur)
+      cur = cur.left
+    }
+    cur = stack.pop()
+    res.push(cur.val)
+    cur = cur.right
+  }
+
+  return res
+};
