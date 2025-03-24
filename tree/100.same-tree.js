21a94be506c5c585249c31a6a75468ba4e39aa61
codersjj <shanewestlife@outlook.com>
2025-03-24 10:22:25 +0800

    feat: 100. Same Tree add another solution

--- tree/100.same-tree.js
+++ tree/100.same-tree.js
@@ -49,3 +49,19 @@ var isSameTree = function(p, q) {
 
   return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
 };
+
+// or:
+
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function(p, q) {
+  if (!p && !q) return true
+  if (p && q && p.val === q.val) {
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+  }
+
+  return false
+};
