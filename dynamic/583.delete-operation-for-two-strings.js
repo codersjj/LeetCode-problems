af7af3c9beea3da8bbd7360040acbf0dd5edc545
codersjj <shanewestlife@outlook.com>
2025-04-10 15:14:44 +0800

    feat: 583. Delete Operation for Two Strings add another solution

--- dynamic/583.delete-operation-for-two-strings.js
+++ dynamic/583.delete-operation-for-two-strings.js
@@ -54,3 +54,31 @@ var minDistance = function(word1, word2) {
 
   return (word1.length - longestCommonLen) + (word2.length - longestCommonLen)
 };
+
+// or:
+
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {number}
+ */
+var minDistance = function(word1, word2) {
+  const dp = Array.from({ length: word1.length + 1 }, () => Array(word2.length + 1))
+
+  for (let i = 0; i <= word1.length; i++) {
+    for (let j = 0; j <= word2.length; j++) {
+      if (i === 0 || j === 0) {
+        dp[i][j] = 0
+        continue
+      }
+      if (word1[i - 1] === word2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1
+      } else {
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1])
+      }
+    }
+  }
+
+  const lcs = dp[word1.length][word2.length]
+  return (word1.length - lcs) + (word2.length - lcs)
+};
