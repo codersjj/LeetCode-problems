39d9a16fca503634638e4bc84c7e9188cfa874ca
codersjj <shanewestlife@outlook.com>
2025-04-27 06:36:13 +0800

    feat: 1797. Design Authentication Manager add another solution

--- interview/1797.design-authentication-manager.js
+++ interview/1797.design-authentication-manager.js
@@ -90,3 +90,59 @@ AuthenticationManager.prototype.countUnexpiredTokens = function(currentTime) {
  * obj.renew(tokenId,currentTime)
  * var param_3 = obj.countUnexpiredTokens(currentTime)
  */
+
+// or:
+
+/**
+ * @param {number} timeToLive
+ */
+var AuthenticationManager = function(timeToLive) {
+  this.timeToLive = timeToLive
+  this.tokens = new Map()
+};
+
+/**
+ * @param {string} tokenId
+ * @param {number} currentTime
+ * @return {void}
+ */
+AuthenticationManager.prototype.generate = function(tokenId, currentTime) {
+  this.tokens.set(tokenId, currentTime + this.timeToLive)
+};
+
+/**
+ * @param {string} tokenId
+ * @param {number} currentTime
+ * @return {void}
+ */
+AuthenticationManager.prototype.renew = function(tokenId, currentTime) {
+  if (!this.tokens.has(tokenId)) return
+  const expiredTime = this.tokens.get(tokenId)
+  if (currentTime < expiredTime) {
+    this.tokens.set(tokenId, currentTime + this.timeToLive)
+  }
+};
+
+/**
+ * @param {number} currentTime
+ * @return {number}
+ */
+AuthenticationManager.prototype.countUnexpiredTokens = function(currentTime) {
+  let count = 0
+
+  for (let expiredTime of this.tokens.values()) {
+    if (currentTime < expiredTime) {
+      count++
+    }
+  }
+
+  return count
+};
+
+/**
+ * Your AuthenticationManager object will be instantiated and called as such:
+ * var obj = new AuthenticationManager(timeToLive)
+ * obj.generate(tokenId,currentTime)
+ * obj.renew(tokenId,currentTime)
+ * var param_3 = obj.countUnexpiredTokens(currentTime)
+ */
