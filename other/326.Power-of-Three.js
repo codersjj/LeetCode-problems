428294f21c7b88ba04b0c186677e67eecc9ee5b6
codersjj <shanewestlife@outlook.com>
2025-05-19 09:39:03 +0800

    feat: 326. Power of Three add other solutions

--- other/326.Power-of-Three.js
+++ other/326.Power-of-Three.js
@@ -44,3 +44,48 @@ var isPowerOfThree = function(n) {
 
   return n === 1
 };
+
+// or:
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfThree = function(n) {
+  if (n <= 0) return false
+
+  while (n % 3 === 0) {
+    n /= 3
+  }
+
+  return n === 1
+};
+
+// or:
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfThree = function(n) {
+  // if n is less than or equal to 0, return false
+  // return whether the log of n divided by the log of 3 is an integer
+
+  if (n <= 0) return false
+
+  const log3 = Math.log(n) / Math.log(3)
+  return Math.abs(log3 - Math.round(log3)) < 1e-10
+};
+
+// or:
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfThree = function(n) {
+  // 3 ** 19 < 2 ** 31 - 1
+  // 3 ** 20 > 2 ** 31 - 1
+  // The largest power of three within the 32-bit integer range is 3^19 = 1162261467. If n is a positive divisor of this number, it must be a power of three. 
+  return n > 0 && 3 ** 19 % n === 0
+};
