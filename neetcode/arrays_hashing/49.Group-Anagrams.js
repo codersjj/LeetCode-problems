92ccf2e8c82c0f886486411016241438eff5e37c
codersjj <shanewestlife@outlook.com>
2025-05-23 05:54:37 +0800

    feat: 49. Group Anagrams add another solution

--- neetcode/arrays_hashing/49.Group-Anagrams.js
+++ neetcode/arrays_hashing/49.Group-Anagrams.js
@@ -138,3 +138,28 @@ var groupAnagrams = function(strs) {
 
   return [...map.values()]
 };
+
+// or:
+
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+  const map = new Map()
+
+  for (const str of strs) {
+    const count = Array(26).fill(0)
+    for (const c of str) {
+      count[c.charCodeAt(0) - 'a'.charCodeAt(0)]++
+    }
+    const key = count.join('#')
+
+    if (!map.has(key)) {
+      map.set(key, [])
+    }
+    map.get(key).push(str)
+  }
+
+  return Array.from(map.values())
+};
