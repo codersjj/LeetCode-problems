6c96337b374db6cd4a456d3f8e1297726e766e43
codersjj <shanewestlife@outlook.com>
2025-06-07 16:41:31 +0800

    feat: 424. Longest Repeating Character Replacement add an optimal solution

--- neetcode/sliding_window/424.Longest-Repeating-Character-Replacement.js
+++ neetcode/sliding_window/424.Longest-Repeating-Character-Replacement.js
@@ -51,3 +51,31 @@ var characterReplacement = function(s, k) {
 
   return res
 };
+
+// or:
+
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var characterReplacement = function(s, k) {
+  let res = 0
+  const count = new Map()
+  let l = 0
+  let maxFreq = 0
+
+  for (let r = 0; r < s.length; r++) {
+    count.set(s[r], (count.get(s[r]) ?? 0) + 1)
+    maxFreq = Math.max(maxFreq, count.get(s[r]))
+
+    while ((r - l + 1) - maxFreq > k) {
+      count.set(s[l], count.get(s[l]) - 1)
+      l++
+    }
+
+    res = Math.max(res, r - l + 1)
+  }
+
+  return res
+};
