b7c5ee6f6edb1d23759f1ece74de4f8007231f07
codersjj <shanewestlife@outlook.com>
2025-06-10 11:15:50 +0800

    feat: 239. Sliding Window Maximum

---
+++ neetcode/sliding_window/239.Sliding-Window-Maximum.js
@@ -0,0 +1,68 @@
+/*
+239. Sliding Window Maximum
+
+You are given an array of integers nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position.
+
+Return the max sliding window.
+
+
+
+Example 1:
+
+Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
+Output: [3,3,5,5,6,7]
+Explanation:
+Window position                Max
+---------------               -----
+[1  3  -1] -3  5  3  6  7       3
+ 1 [3  -1  -3] 5  3  6  7       3
+ 1  3 [-1  -3  5] 3  6  7       5
+ 1  3  -1 [-3  5  3] 6  7       5
+ 1  3  -1  -3 [5  3  6] 7       6
+ 1  3  -1  -3  5 [3  6  7]      7
+Example 2:
+
+Input: nums = [1], k = 1
+Output: [1]
+
+
+Constraints:
+
+1 <= nums.length <= 105
+-104 <= nums[i] <= 104
+1 <= k <= nums.length
+*/
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var maxSlidingWindow = function(nums, k) {
+  const res = []
+  const queue = [] // index
+  let l = 0
+  let r = 0
+
+  while (r < nums.length) {
+    // pop smaller values from queue
+    while (queue.length && nums[queue.at(-1)] < nums[r]) {
+      queue.pop()
+    }
+    queue.push(r)
+
+    // remove left value from window
+    if (l > queue[0]) {
+      queue.shift()
+    }
+
+    if (r + 1 >= k) {
+      res.push(nums[queue[0]])
+      l++
+    }
+
+    r++
+  }
+
+  return res
+};
