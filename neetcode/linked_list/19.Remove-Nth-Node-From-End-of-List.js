157f15366c77e7c64e3f6c4a8ff6d59c1036da82
codersjj <shanewestlife@outlook.com>
2025-06-12 10:19:42 +0800

    feat: add a neetcode problem

---
+++ neetcode/linked_list/19.Remove-Nth-Node-From-End-of-List.js
@@ -0,0 +1,7 @@
+/*
+19. Remove Nth Node From End of List
+
+link: https://leetcode.com/problems/remove-nth-node-from-end-of-list/description/
+
+see: linked-list/19.remove-Nth-node-from-end-of-list.js
+*/
