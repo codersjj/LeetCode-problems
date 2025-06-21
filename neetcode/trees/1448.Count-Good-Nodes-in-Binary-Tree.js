ba1891359f0330baba815eca47296c36e9266a68
codersjj <shanewestlife@outlook.com>
2025-06-21 16:08:07 +0800

    feat: 1448. Count Good Nodes in Binary Tree

---
+++ neetcode/trees/1448.Count-Good-Nodes-in-Binary-Tree.js
@@ -0,0 +1,88 @@
+/*
+1448. Count Good Nodes in Binary Tree
+
+Given a binary tree root, a node X in the tree is named good if in the path from root to X there are no nodes with a value greater than X.
+
+Return the number of good nodes in the binary tree.
+
+
+
+Example 1:
+
+
+
+Input: root = [3,1,4,3,null,1,5]
+Output: 4
+Explanation: Nodes in blue are good.
+Root Node (3) is always a good node.
+Node 4 -> (3,4) is the maximum value in the path starting from the root.
+Node 5 -> (3,4,5) is the maximum value in the path
+Node 3 -> (3,1,3) is the maximum value in the path.
+Example 2:
+
+
+
+Input: root = [3,3,null,4,2]
+Output: 3
+Explanation: Node 2 -> (3, 3, 2) is not good, because "3" is higher than it.
+Example 3:
+
+Input: root = [1]
+Output: 1
+Explanation: Root is considered as good.
+
+
+Constraints:
+
+The number of nodes in the binary tree is in the range [1, 10^5].
+Each node's value is between [-10^4, 10^4].
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var goodNodes = function(root) {
+  function dfs(node, maxVal) {
+    if (!node) return 0
+
+    maxVal = Math.max(maxVal, node.val)
+    const left = dfs(node.left, maxVal)
+    const right = dfs(node.right, maxVal)
+
+    const res = node.val >= maxVal ? 1 : 0
+
+    return res + left + right
+  }
+
+  return dfs(root, root.val)
+};
+
+// or:
+
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var goodNodes = function(root) {
+  function dfs(node, maxVal) {
+    if (!node) return 0
+
+    let res = node.val >= maxVal ? 1 : 0
+    maxVal = Math.max(maxVal, node.val)
+    res += dfs(node.left, maxVal)
+    res += dfs(node.right, maxVal)
+
+    return res
+  }
+
+  return dfs(root, root.val)
+};
