c051002ee5261a9d2cc6f8dc612399f16e784633
codersjj <shanewestlife@outlook.com>
2025-06-22 14:59:05 +0800

    feat: add neetcode problems

---
+++ neetcode/trees/230.Kth-Smallest-Element-in-a-BST.js
@@ -0,0 +1,7 @@
+/*
+230. Kth Smallest Element in a BST
+
+link: https://leetcode.com/problems/kth-smallest-element-in-a-bst/description/
+
+see: tree/230.kth-smallest-element-in-a-bst.js
+*/
---
+++ neetcode/trees/98.Validate-Binary-Search-Tree.js
@@ -0,0 +1,7 @@
+/*
+98. Validate Binary Search Tree
+
+link: https://leetcode.com/problems/validate-binary-search-tree/description/
+
+see: tree/98.validate-binary-search-tree.js
+*/
